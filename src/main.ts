5d5095fa6aa6dfda7ad406ae52df648dd530d276
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2025-04-24 17:26:26 -0300

    💫feat: Atualização dos pacotes + Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+
+  //DOCS - SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('TAYA')
+    .setDescription('API Service')
+    .setVersion('1.0')
+    .addTag('taya')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.setup('api', app, documentFactory);
+
+  //DOCS - SCALAR
+  app.use(
+    '/scalar',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
+
+  await app.listen(3005, () => {
+    console.log('-----------------------');
+    console.log('🎉 - Servidor Online !');
+    console.log('\x1b[34m%s\x1b[0m', '🔗 - http://localhost:3005');
+    console.log('\x1b[33m%s\x1b[0m', '📃 - http://localhost:3005/api');
+    console.log('\x1b[33m%s\x1b[0m', '📃 - http://localhost:3005/scalar');
+    console.log('-----------------------');
+  });
 }
 bootstrap();
