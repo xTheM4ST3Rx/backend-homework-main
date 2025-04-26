15f2d344703d30b48faf403ba11ce1beb4d80141
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2025-04-26 12:23:04 -0300

    💫feat: agora todas as API'S tem que usar user_id

--- src/middlewares/get-user-middleware.ts
+++ src/middlewares/get-user-middleware.ts
@@ -1,4 +1,8 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request, Response, NextFunction } from 'express';
 import { Repository } from 'typeorm';
@@ -13,11 +17,12 @@ export class UserMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     const userId = req.headers['user_id'];
-    if (userId) {
-      const user = await this.userRepository.findOneBy({ id: Number(userId) });
-      if (user) {
-        (req as any).user = user;
-      }
+    if (!userId) {
+      throw new UnauthorizedException('Header user_id é obrigatório');
+    }
+    const user = await this.userRepository.findOneBy({ id: Number(userId) });
+    if (user) {
+      (req as any).user = user;
     }
     next();
   }
